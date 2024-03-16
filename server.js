eb1810f71884a6ba86e76b23f00b7847303b9d3a
Ravisankar.Ka <ka.ravisankar@gmail.com>
2024-03-16 13:33:26 +0530

    Swagger Documentaion

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import swagger from "swagger-ui-express";
+import apiDocs from "./swagger.json" assert { type: "json" };
 import { connectToDatabase } from "./config/dbConnection.js";
 import { DoctorRoutes } from "./src/features/doctor/routes/doctorRoutes.js";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 import { PatientRoutes } from "./src/features/patient/routes/patientRoutes.js";
 import { JwtAuth } from "./middlewares/jwtAuth.js";
 import { ReportRoutes } from "./src/features/report/routes/reportRoutes.js";
+import { assert } from "console";
+import { type } from "os";
 
 const server = express();
 server.use(express.urlencoded({ extended: true }));
@@ -16,15 +20,29 @@ server.use(express.json());
 const configPath = path.resolve("config", "uat.env");
 dotenv.config({ path: configPath });
 
+server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
+
 // default path
 server.get("/", (req, res) => {
-  res.status(200).send("Hospital API");
+  res.status(200).json({
+    message: "Welcome to hospital API",
+    API_Documentation: `http://localhost:${process.env.PORT}/api-docs`,
+  });
 });
 
 // Routes
 server.use("/api/v1/doctors", DoctorRoutes);
 server.use("/api/v1/patients", JwtAuth, PatientRoutes);
 server.use("/api/v1/reports", JwtAuth, ReportRoutes);
+
+// handling 404 on API path
+server.use("*", (req, res) => {
+  res.status(404).json({
+    error: "The requested path is not available",
+    message: "Please refer to the below or /api-docs path for more information",
+    API_Documentation: `http://localhost:${process.env.PORT}/api-docs`,
+  });
+});
 
 // Error handler for global usage
 server.use(errorHandlerMiddleware);
